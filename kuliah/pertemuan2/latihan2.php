26cad395821f4a185e07700b35b654495fc5b776
TastaftianGanda <permanaganda109@gmail.com>
2025-02-17 19:24:00 +0700

    Add files via upload

---
+++ kuliah/pertemuan2/latihan2.php
@@ -0,0 +1,10 @@
+<?php
+    $nama = "Tastaftian Ganda";
+    $email = "permanaganda109@gmail.com";
+    $pekerjaan = "Mahasiswa";
+    $jurusan = "jurusan Teknik Informatika";
+    $universitas = "Universitas Pasundan";
+    echo "<h1>$nama</h1>";
+    echo "<h4>$email</h4>";
+    echo "<p>Halo, nama saya $nama Saya adalah seorang $pekerjaan di $universitas $jurusan dan semester 2 </p>";
+?>
